9fdf966d846444f88ea9ac6642e90ff207298304
Andrew Johnson <ajohnson.uva@gmail.com>
2018-12-11 20:07:06 -0500

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -7,11 +7,67 @@ class Game
     [0, 4, 8], [2, 4, 6]
   ]
 
-  def initialize
-    self.board = Board.new
-    self.player_1 = Players::Human.new
-    self.player_2 = Players::Human.new
+  def initialize (p1 = nil, p2 = nil, board = nil)
+    p1 ? @player_1 = p1 : @player_1 = Players::Human.new("X")
+    p2 ? @player_2 = p2 : @player_2 = Players::Human.new("O")
+    board ? @board = board : @board = Board.new
   end
 
+  def board
+    @board
+  end
+
+  def current_player
+    self.board.turn_count.even? ? self.player_1 : self.player_2
+  end
+
+  def won? #if win, return winning indices, otherwise return false
+    x = WIN_COMBINATIONS.select do |array|
+      array.all? {|i| self.board.cells[i] == "X"} || array.all? {|i| self.board.cells[i] == "O"}
+    end
+    x.empty? ? false : x.flatten
+  end
+
+  def draw?
+    self.board.full? && !self.won?
+  end
+
+  def over?
+    self.draw? || self.won?
+  end
+
+  def winner
+    if self.won?
+      self.board.cells[self.won?[0]]
+    else
+      nil
+    end
+  end
+
+  def turn
+    puts "Where do you want to move?"
+    input = self.current_player.move(board)
+    position = self.board.position(input)
+    if !(self.board.taken?(position))
+      self.board.update(position, self.current_player)
+    else
+      "invalid"
+    end
+  end
 
 end
+
+
+
+
+
+#save for AI
+
+# moved = false
+# until moved == true
+#   x = rand(1..9)
+#   if !(self.board.taken?(rand(1..9)))
+#     self.board.update(x, self.player)
+#     moved = true
+#   end
+# end
